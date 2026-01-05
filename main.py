42d4342bd3946f30a2554080cff9b59b4d21fbb9
reedinkenbrandt-spec <reed.inkenbrandt@gmail.com>
2026-01-04 16:23:13 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,95 @@
+
+
+teams = [
+    {   
+        "name" : "Crows",
+        "players" : {
+            "pitcher": { 
+                "name" : "Kobe Loft",
+                "batting average": 217,
+                "ERA" : 2.02
+            },
+            "catcher": { 
+                "name" : "Dave jacobs",
+                "batting average": 349
+            },
+            "1base": { 
+                "name" : "Al Hae",
+                "batting average": 301
+            },
+            "2base": { 
+                "name" : " Donvan Kak",
+                "batting average": 183
+            },
+            "3base": { 
+                "name" : "Kail Jay",
+                "batting average": 270
+            },
+            "shortstop": { 
+                "name" : "Jalen Sev",
+                "batting average": 297
+            },
+            "right": { 
+                "name" : "Shaquille Smaesh",
+                "batting average": 445
+            },
+            "center": { 
+                "name" : "Ishawn Holland",
+                "batting average": 263
+            },
+            "left": { 
+                "name" : "Tevin James",
+                "batting average": 274
+            }
+        }
+    },
+    {  
+        "name" : "Jumps",
+        "players" : {
+            "pitcher": { 
+                "name" : "Jalen Burst",
+                "batting average": 174,
+                "ERA" : 3.12
+            },
+            "catcher": { 
+                "name" : "Klay Jones",
+                "batting average": 247
+            },
+            "1base": { 
+                "name" : "Tommy Webb",
+                "batting average": 354
+            },
+            "2base": { 
+                "name" : " Dak Presser",
+                "batting average": 194
+            },
+            "3base": { 
+                "name" : "Dave Jonas",
+                "batting average": 253
+            },
+            "shortstop": { 
+                "name" : "Jason Wake",
+                "batting average": 297
+            },
+            "right": { 
+                "name" : "Devin Sheesh",
+                "batting average": 339
+            },
+            "center": { 
+                "name" : "Shawn Holot",
+                "batting average": 272
+            },
+            "left": { 
+                "name" : "Kai Jose",
+                "batting average": 312
+            }
+        }
+    }
+]
+
+
+
+print("Baseball game simulation v0.01")
+print(f'Home Team: {teams[0]["name"]}    Away Team: {teams[1]["name"]}')
+
+
